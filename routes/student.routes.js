73d36d4a08d779d213450af9a103d485d4fb165b
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-08-15 18:35:08 +0530

    initial implementation of student post api route

---
+++ routes/student.routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+const { body } = require('express-validator');
+
+const StudentControllers = require('../controller/student.controller');
+
+const Student = require('../schemas/student.schema');
+
+const router = express.Router();
+
+router.post(
+	'/students',
+	[
+		body('firstName', 'FirstName must contain chars only').trim().isString(),
+		body('lastName', 'LastName must contain chars only').trim().isString(),
+		body('gender', 'Gender must be contain chars only'),
+		body('age', 'Insert age!').trim().notEmpty(),
+		body('email', 'Email must be a valid email').trim().isEmail().custom(async (value, { req }) => {
+		})
+	],
+	StudentControllers.addStudent
+);
+
+module.exports = router;
