4406159dcc265e34bbfb7bfb899f5070e795ee42
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-08-15 20:58:13 +0530

    update student routes

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -16,11 +16,29 @@ router.post(
 		body('gender', 'Gender must be contain chars only'),
 		body('age', 'Insert age!').trim().notEmpty(),
 		body('email', 'Email must be a valid email').trim().isEmail().custom(async (value, { req }) => {
+			try {
+				const student = await Student.getStudentWithCondition({ email: value });
+				if (student) {
+					return Promise.reject('This email is taken by some student');
+				}
+				// const teacher = await Teacher.getTeacherWithCondition({ email: value });
+				// if (teacher) {
+				// 	return Promise.reject('This Email is taken by some teacher');
+				// }
+				return true;
+			} catch (error) {
+				error.statusCode = 500;
+				throw error;
+			}
 		})
 	],
 	StudentControllers.addStudent
 );
 
 router.get('/students', StudentControllers.getAllStudents);
+
+router.get('/student/:id', StudentControllers.getStudent);
+
+router.get('/students/search/:text', StudentControllers.getSearchForStudents);
 
 module.exports = router;
