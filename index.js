98b03b0aeb957babdd41d00905c1cfcd46849e57
SJayasinghe <it19126234@my.sliit.lk>
2021-08-18 08:17:26 -0700

    Post teacher details

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const initDb = require('./helpers/db').initDb;
 
 const StudentRoutes = require('./routes/student.routes');
+const TeacherRoutes = require('./routes/teacher.routes');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -14,6 +15,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 
 app.use('/api/v1', StudentRoutes);
+app.use('/api/v1', TeacherRoutes);
 
 app.use((error, req, res, next) => {
 	const errorMessage = error.message ? error.message : 'something went wrong';
