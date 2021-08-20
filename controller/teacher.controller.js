39adc3c8168507f20d056c34a47fe2a7c159152e
SJayasinghe <it19126234@my.sliit.lk>
2021-08-20 09:41:00 -0700

    create search teacher API end point-AB#47

--- controller/teacher.controller.js
+++ controller/teacher.controller.js
@@ -2,6 +2,7 @@ const { validationResult } = require('express-validator');
 const { ObjectId } = require('mongodb');
 
 const Teacher = require('../schemas/teacher.schema');
+const Subject = require('../schemas/Subject.schema');
 
 exports.postAddTeacher = async (req, res, next) => {
 	const { firstName, lastName, age, email, salary, gender, subjectId } = req.body;
@@ -16,13 +17,13 @@ exports.postAddTeacher = async (req, res, next) => {
 		}
 
 		// check if the subject exists
-		// const foundSubject = await Subject.getSubject(subjectId);
+		const foundSubject = await Subject.getSubject(subjectId);
 
-		// if (!foundSubject) {
-		// 	const error = new Error('no subject with that id exist');
-		// 	error.statusCode = 404;
-		// 	throw error;
-		// }
+		if (!foundSubject) {
+			const error = new Error('no subject with that id exist');
+			error.statusCode = 404;
+			throw error;
+		}
 
 		const teacher = new Teacher(
 			firstName,
@@ -38,10 +39,10 @@ exports.postAddTeacher = async (req, res, next) => {
 		const { insertedId } = await teacher.addTeacher();
 
 		// add the teacher id to the teachers array in that subject
-		// await Subject.updateSubjectWithConfigs(
-		// 	{ _id: new ObjectId(subjectId) },
-		// 	{ $addToSet: { teachers: insertedId } }
-		// );
+		await Subject.updateSubjectWithConfigs(
+			{ _id: new ObjectId(subjectId) },
+			{ $addToSet: { teachers: insertedId } }
+		);
 
 		res.status(201).json({ message: 'Teacher added successfully', teacherId: insertedId.toString() });
 	} catch (error) {
@@ -53,6 +54,17 @@ exports.postAddTeacher = async (req, res, next) => {
 exports.getGetTeachers = async (req, res, next) => {
 	try {
 		const teachers = await Teacher.getTeachers();
+		res.status(200).json({ teachers: teachers });
+	} catch (error) {
+		if (!error.statusCode) error.statusCode = 500;
+		next(error);
+	}
+};
+
+exports.getSearchForTeachers = async (req, res, next) => {
+	const searchText = req.params.text;
+	try {
+		const teachers = await Teacher.searchForTeacher(searchText);
 		res.status(200).json({ teachers: teachers });
 	} catch (error) {
 		if (!error.statusCode) error.statusCode = 500;
